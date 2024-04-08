72b7f412a6afce3e4dd2e3da00affc2481d21810
hieudinh2002 <dinhvanhieu2002@gmail.com>
2024-04-08 22:44:31 +0700

    add plant shooting

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -2,12 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Sprite[] images; // Array of images to cycle through
+    [SerializeField] private Image imageDisplay;
+    [SerializeField] private Button nextButton;
+    [SerializeField] private Button previousButton;
+    [SerializeField] private Button playButton;
+    private int currentIndex = 0;
+
+    private void Awake()
+    {
+        nextButton.onClick.AddListener(NextImage);
+        previousButton.onClick.AddListener(PreviousImage);
+    }
+    private void Start()
+    {
+        DisplayImage();
+
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void DisplayImage()
+    {
+        imageDisplay.sprite = images[currentIndex];
+    }
+
+    public void NextImage()
+    {
+        if(currentIndex < images.Length - 1)
+        {
+            currentIndex++;
+        }
+        DisplayImage();
+    }
+
+    public void PreviousImage()
+    {
+        if (currentIndex > 0)
+        {
+            currentIndex--;
+        }
+        DisplayImage();
     }
 }
