6b20e26923d8e826330604bfb3ace592c01f8e90
nhuyluong <ltny.0502@gmail.com>
2024-05-06 22:43:38 +0700

    handle enemies can attack

---
+++ Assets/Scripts/MemeEnemy.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Meme : MonoBehaviour
+{
+    [Header("Attack Parameters")]
+    [SerializeField] private float attackColldown;
+    [SerializeField] private float range;
+    [SerializeField] private int damage;
+
+    [Header("Collider ParaMeters")]
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private BoxCollider2D boxCollider;
+
+    [Header("Player Layer")]
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
+
+    //References
+    private Animator anim;
+    private PlayerLife playerLife;
+
+    private EnemyPatrol enemyPatrol;
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        enemyPatrol = GetComponentInParent<EnemyPatrol>();
+    }
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+
+        //Attack only when phayer in sight?
+        if (PlayerInSight())
+        {
+            if (cooldownTimer >= attackColldown)
+            {
+                cooldownTimer = 0;
+                anim.SetTrigger("memeAttack");
+            }
+        }
+
+        if (enemyPatrol != null)
+            enemyPatrol.enabled = !PlayerInSight();
+       
+    }
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            0, Vector2.left, 0, playerLayer);
+
+        if(hit.collider != null)
+            playerLife = hit.transform.transform.GetComponent<PlayerLife>();
+
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+
+    private void DamagePlayer()
+    {
+        //If player still in range dame him
+        if (PlayerInSight())
+        {
+            //playerLife.Die();
+        }
+    }
+}
